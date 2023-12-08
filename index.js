a2cbce6dfe6301ba52f3359a22ad1f9e6e9ece4a
hererr <153145567+hererr@users.noreply.github.com>
2023-12-08 16:09:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+app.get('/:col', (req, res) => {
+    fs.readFile(`${req.params.col}/lb.json`, "utf8", (err, jsonString) => {
+        res.send(jsonString)
+    });
+})
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
